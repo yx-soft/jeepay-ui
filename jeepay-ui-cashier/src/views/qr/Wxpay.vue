adc3370f0c640f2afa7f85201dcc9a4d1ead058d
gao.xy <xingyungao@hotmail.com>
2022-09-22 20:44:00 +0800

    微信收银台

--- jeepay-ui-cashier/src/views/qr/Wxpay.vue
+++ jeepay-ui-cashier/src/views/qr/Wxpay.vue
@@ -148,7 +148,7 @@ export default {
 
       // eslint-disable-next-line no-undef
       WeixinJSBridge.invoke(
-          'getBrandWCPayRequest', JSON.parse(that.resData.payInfo),
+          'getBrandWCPayRequest', JSON.parse(that.resData.payData),
           function(res) {
             if (res.err_msg == "get_brand_wcpay_request:ok") {
               // 使用以上方式判断前端返回,微信团队郑重提示：
